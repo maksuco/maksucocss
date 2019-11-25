a8c43c6dd0af075946ec2195ce0575d07902b3fb
maksuco <mm@maksuco.com>
2019-11-25 17:24:46 -0500

    up

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   content: ['*.html', '*.blade', '*.blade.php', '*.php', '*/*.php', '*.js', '*/*.js','frontend-min.js'],
-  css: ['compiled.css'],
+  css: ['maksucocss.css'],
   whitelist: ['body','fa','fab','fas','fal','far','fa-instagram'],
-  whitelistPatterns: [/fa-/]
+  //whitelistPatterns: [/fa-/]
 }
 //purgecss --config ./purgecss.config.js --out css
